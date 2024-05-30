435e9d1cc81bb9dc4986b94ba1cf533e55a79e0c
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-05-30 21:45:24 +0200

    Test on no LOAD_TESTS

--- extension_config.cmake
+++ extension_config.cmake
@@ -8,14 +8,12 @@ duckdb_extension_load(autocomplete
 duckdb_extension_load(httpfs
     SOURCE_DIR extension/httpfs
     INCLUDE_DIR extension/httpfs
-    LOAD_TESTS
     DONT_LINK
     LINKED_LIBS "../../third_party/mbedtls/libduckdb_mbedtls.a;../../vcpkg_installed/wasm32-emscripten/lib/libcrypto.a;../../vcpkg_installed/wasm32-emscripten/lib/libssl.a"
 )
 
     duckdb_extension_load(azure
 		DONT_LINK
-            LOAD_TESTS
             GIT_URL https://github.com/carlopi/duckdb_azure
             GIT_TAG main
     LINKED_LIBS "../../third_party/mbedtls/libduckdb_mbedtls.a;../../vcpkg_installed/wasm32-emscripten/lib/libcrypto.a;../../vcpkg_installed/wasm32-emscripten/lib/libssl.a;../../vcpkg_installed/wasm32-emscripten/lib/libazure-identity.a"
