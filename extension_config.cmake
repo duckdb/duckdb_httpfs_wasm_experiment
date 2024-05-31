748feb480987ecd3419bc69c350a6ea0295af3a1
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-05-31 11:10:25 +0200

    Spatial love

--- extension_config.cmake
+++ extension_config.cmake
@@ -18,4 +18,12 @@ duckdb_extension_load(httpfs
 #            GIT_TAG main
 #    LINKED_LIBS "../../third_party/mbedtls/libduckdb_mbedtls.a;../../vcpkg_installed/wasm32-emscripten/lib/libcrypto.a;../../vcpkg_installed/wasm32-emscripten/lib/libssl.a;../../vcpkg_installed/wasm32-emscripten/lib/libazure-identity.a"
 #            )
+
 	
+duckdb_extension_load(spatial
+    DONT_LINK LOAD_TESTS
+    GIT_URL https://github.com/duckdb/duckdb_spatial.git
+    GIT_TAG 6826755ec32aea77777b136cc81683fa9be4df7b
+    INCLUDE_DIR spatial/include
+    TEST_DIR test/sql
+    )
