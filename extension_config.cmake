113d595d091b5830487489c000f20635d67433d3
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-09-16 15:13:58 +0200

    Do run httpfs

--- extension_config.cmake
+++ extension_config.cmake
@@ -5,12 +5,12 @@ duckdb_extension_load(autocomplete
 	DONT_LINK
 )
 #
-#duckdb_extension_load(httpfs
-#    SOURCE_DIR extension/httpfs
-#    INCLUDE_DIR extension/httpfs
-#    DONT_LINK
-#    LINKED_LIBS ../../third_party/mbedtls/libduckdb_mbedtls.a ../../vcpkg_installed/wasm32-emscripten/lib/libcrypto.a ../../vcpkg_installed/wasm32-emscripten/lib/libssl.a
-#)
+duckdb_extension_load(httpfs
+    SOURCE_DIR extension/httpfs
+    INCLUDE_DIR extension/httpfs
+    DONT_LINK
+    LINKED_LIBS ../../third_party/mbedtls/libduckdb_mbedtls.a ../../vcpkg_installed/wasm32-emscripten/lib/libcrypto.a ../../vcpkg_installed/wasm32-emscripten/lib/libssl.a
+)
 
 #   duckdb_extension_load(azure
 #		DONT_LINK
@@ -19,15 +19,15 @@ duckdb_extension_load(autocomplete
 #    LINKED_LIBS "../../third_party/mbedtls/libduckdb_mbedtls.a;../../vcpkg_installed/wasm32-emscripten/lib/libcrypto.a;../../vcpkg_installed/wasm32-emscripten/lib/libssl.a;../../vcpkg_installed/wasm32-emscripten/lib/libazure-identity.a"
 #            )
 
-	
-duckdb_extension_load(spatial
-    DONT_LINK LOAD_TESTS
-    GIT_URL https://github.com/carlopi/duckdb_spatial.git
-    GIT_TAG 5720575fafe6b10748a8d2172a72eb93182f59f6
-    INCLUDE_DIR spatial/include
-    TEST_DIR test/sql
-	LINKED_LIBS '../../deps/local/lib/*.a'
-    )
+#	
+#duckdb_extension_load(spatial
+#    DONT_LINK LOAD_TESTS
+#    GIT_URL https://github.com/carlopi/duckdb_spatial.git
+#    GIT_TAG 5720575fafe6b10748a8d2172a72eb93182f59f6
+#    INCLUDE_DIR spatial/include
+#    TEST_DIR test/sql
+#	LINKED_LIBS '../../deps/local/lib/*.a'
+#    )
 
 duckdb_extension_load(json DONT_LINK)
 duckdb_extension_load(parquet DONT_LINK)
