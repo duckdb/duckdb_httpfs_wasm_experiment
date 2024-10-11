9c7b2dc8a65308051eb6b972cd6aaf36a9fdf0ef
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-10-11 13:25:11 +0200

    Also httpfs

--- extension_config.cmake
+++ extension_config.cmake
@@ -2,12 +2,12 @@
 
 # Extension from this repo
 #
-#duckdb_extension_load(httpfs
-#    DONT_LINK
-#    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/extension/httpfs
-#    INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/extension/httpfs/include
-#    LINKED_LIBS ../../third_party/mbedtls/libduckdb_mbedtls.a ../../vcpkg_installed/wasm32-emscripten/lib/libcrypto.a 
-#)
+duckdb_extension_load(httpfs
+    DONT_LINK
+    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/extension/httpfs
+    INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/extension/httpfs/include
+    LINKED_LIBS ../../third_party/mbedtls/libduckdb_mbedtls.a ../../vcpkg_installed/wasm32-emscripten/lib/libcrypto.a 
+)
 
 #   duckdb_extension_load(azure
 #		DONT_LINK
